da1b5b018d07e9a3e3d00490d2243be773f3646d
jamesou9307 <291387044@qq.com>
2017-08-03 16:51:39 +0800

    for activity launch commit

---
+++ app/src/main/java/com/james/firstactivity/FifthActivity.java
@@ -0,0 +1,13 @@
+package com.james.firstactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FifthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifth_activity_layout);
+    }
+}
